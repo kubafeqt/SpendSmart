7688670a533b298666198c7c2af64dc60f18606e
Kubaf Jedoma <bukajcz.kubaf@seznam.cz>
2025-04-29 10:11:47 +0200

    Add project files.

---
+++ SpendSmart/Controllers/HomeController.cs
@@ -0,0 +1,83 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using SpendSmart.Models;
+
+namespace SpendSmart.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+   private readonly SpendSmartDbContext _context;
+
+   public HomeController(ILogger<HomeController> logger, SpendSmartDbContext context)
+    {
+        _logger = logger;
+      _context = context;
+    }
+
+
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+   public IActionResult Expenses()
+   {
+      var allExpenses = _context.Expenses.ToList();
+
+      var totalExpenses = allExpenses.Sum(x => x.Value);
+
+      ViewBag.Expenses = totalExpenses;
+
+      return View(allExpenses);
+   }
+
+   public IActionResult CreateEditExpense(int? id)
+   {
+      if (id != null)
+      {
+         // editing -> load an expenses by id
+         var expensesInDb = _context.Expenses.SingleOrDefault(x => x.Id == id);
+         return View(expensesInDb);
+      }
+      
+      return View();
+   }
+
+   public IActionResult DeleteExpense(int id)
+   {
+      var expensesInDb = _context.Expenses.SingleOrDefault(x => x.Id == id);
+      _context.Expenses.Remove(expensesInDb);
+      _context.SaveChanges();
+      return RedirectToAction("Expenses");
+   }
+
+   public IActionResult CreateEditExpenseForm(Expense model)
+   {
+      if (model.Id == 0)
+      {
+         // Create
+         _context.Expenses.Add(model);
+      }
+      else
+      {
+         // Editing
+         _context.Expenses.Update(model);
+      }
+      _context.SaveChanges();
+      return RedirectToAction("Expenses");
+   }
+
+   public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
